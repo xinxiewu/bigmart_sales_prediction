a284425c050948f8658246ccb806476335b94726
wuxinxie <xinxiewu@seas.upenn.edu>
2023-08-01 23:33:39 -0400

    EDA 2

--- code/__main__.py
+++ code/__main__.py
@@ -6,8 +6,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from util import *
 
-sns.set()
-
 def main(fileurl=None, fileurl_unseen=None, output=None, target=None, discrete_x=None, continuos_x=None):
     """
     Step 1: Data Preparation & EDA
@@ -17,11 +15,27 @@ def main(fileurl=None, fileurl_unseen=None, output=None, target=None, discrete_x
     df_unseen = download_file(fileurl_unseen)
     # 1.2 EDA
     print(f"Loaded modeling data {df.shape} and unseen data {df_unseen.shape}. \n\nEDA Starts")
-    ## (a) Feature Distribution (Extreme Value) & Missing Value Detection
+    ## (a) Feature Distribution, Extreme & Missing Value Detection
+    missing, categorical = feature_distribution(df=df, discrete_x=discrete_x, continuos_x=continuos_x, target=target)
+    print(f"Features with missing values: {missing}\nCategorical features: {categorical}")
+    ## (b) Feature Engineering 1: Missing Value & Categorical -> Numeric
+    """
+    Missing 1: Item_Weight - Same Item_Identifier has the same weight
+    Missing 2: Outlet_Size - Grocery Store is Small; Supermarket Type 1 is ?
+    Missing 3: Item_Visibility - Item_Identifier; Outlet_Identifier -> Item_Type -> Item_Fat_Content -> Avg
+    """
+    df = missing_handler(df=df, missing=missing, output=output)
+    """ Categorical -> Numeric
+    Item_Identifier:
+    Item_Fat_Content:
+    Item_Type:
+    Outlet_Identifier:
+    Outlet_Size:
+    Outlet_Location_Type:
+    Outlet_Type:
 
-    
-    
-
+    """
+    df = categorical_conversion(df=df, categorical_feature=categorical)
 
 
     return
@@ -35,11 +49,11 @@ if __name__ == '__main__':
     """
     Step 2: Call the main program
     """
-    # main(fileurl = 'https://raw.githubusercontent.com/xinxiewu/datasets/main/bigmart_sales/Train.csv',
-    #      fileurl_unseen = 'https://raw.githubusercontent.com/xinxiewu/datasets/main/bigmart_sales/Test.csv',
-    #      output = r'../public/output',
-    #      target = 'Item_Outlet_Sales',
-    #      discrete_x = ('Item_Identifier', 'Item_Fat_Content', 'Item_Type', 'Outlet_Identifier', 
-    #                       'Outlet_Establishment_Year', 'Outlet_Size', 'Outlet_Location_Type', 'Outlet_Type'),
-    #      continuos_x = ('Item_Weight', 'Item_Visibility', 'Item_MRP')
-    #      )
+    main(fileurl = 'https://raw.githubusercontent.com/xinxiewu/datasets/main/bigmart_sales/Train.csv',
+         fileurl_unseen = 'https://raw.githubusercontent.com/xinxiewu/datasets/main/bigmart_sales/Test.csv',
+         output = r'../public/output',
+         target = 'Item_Outlet_Sales',
+         discrete_x = ('Item_Identifier', 'Item_Fat_Content', 'Item_Type', 'Outlet_Identifier', 
+                          'Outlet_Establishment_Year', 'Outlet_Size', 'Outlet_Location_Type', 'Outlet_Type'),
+         continuos_x = ('Item_Weight', 'Item_Visibility', 'Item_MRP')
+         )
--- code/util.py
+++ code/util.py
@@ -3,10 +3,14 @@ util.py contains custom functions:
     1. download_file: Download the .csv file from the given link and read as dataframe
     2. delete_files: Delete files in the given folder, except README.md
     3. feature_distribution: Generate plots of features distribution and save
+    4. missing_handler: Handle missing values
+    5. categorical_conversion: Convert categorical to numeric
 """
 import requests
 import pandas as pd
 import os
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # download_file(url, output)
 def download_file(url=None, output=r'../public/output'):
@@ -44,7 +48,7 @@ def delete_files(path=r'../public/output', keep=['README.md']):
     return
 
 # feature_distribution(discrete_x, continuos_x, output)
-def feature_distribution(df=None, discrete_x=None, continuos_x=None, target=None, 
+def feature_distribution(df=None, discrete_x=None, continuos_x=None, target=None, fname='variables_dist',
                          output=r'../public/output', subplots=[4,3], figsize=(30,30)):
     """ Generate plots of features distribution and save
 
@@ -58,6 +62,59 @@ def feature_distribution(df=None, discrete_x=None, continuos_x=None, target=None
         figsize: figure size
 
     Returns:
-        nothing to return
+        missing: set, variables with missing values
+        categorical: set, variables with categorical
     """
-    pass
+    missing, categorical = set(), set()
+    sns.set()
+    fig, axes = plt.subplots(subplots[0], subplots[1], figsize=figsize)
+    ax = axes.flatten()
+    for i, col in enumerate(df.columns):
+        if col in discrete_x:
+            data_type, data_cat = 'Discrete', 'Numeric' if col == 'Outlet_Establishment_Year' else 'Categorical'
+            if data_cat == 'Categorical':
+                categorical.add(col)
+            miss = df[col].isna().value_counts()[True] if True in df[col].isna().value_counts().index else 0
+            if miss > 0:
+                missing.add(col)
+            if col == 'Item_Identifier':
+                disct = sns.countplot(data=pd.DataFrame(df[col].value_counts()), x=col,ax=ax[i])
+                disct.set(xlabel=f"Appearance of {col}: {data_type}+{data_cat}", ylabel='Count of Items', title=f"{col} - {miss} Missing")
+            else:
+                disct = sns.countplot(data=df, x=col,ax=ax[i])
+                disct.set(xlabel=f"{col}: {data_type}+{data_cat}", ylabel='Count', title=f"{col} - {miss} Missing")
+                if col in ('Item_Type', 'Outlet_Identifier', 'Outlet_Establishment_Year'):
+                    disct.set_xticklabels(disct.get_xticklabels(), rotation=30, fontsize=10)
+        elif col in continuos_x or col == target:
+            data_type, data_cat = 'Continuous', 'Numeric'
+            if col == 'Item_Visibility':
+                miss = (getattr(df,col)==0).value_counts()[True] if True in (getattr(df,col)==0).value_counts().index else 0
+                cont = sns.histplot(df[df[col]!=0][col],kde=True,color='purple',ax=ax[i])
+            else:
+                miss = df[col].isna().value_counts()[True] if True in df[col].isna().value_counts().index else 0
+                cont = sns.histplot(getattr(df,col),kde=True,color='purple',ax=ax[i])
+            cont.set(xlabel=f"{col}: {data_type}+{data_cat}", ylabel='Density', title=f"{col} - {miss} Missing")
+            if miss > 0:
+                missing.add(col)
+    plt.tight_layout()
+    plt.savefig(os.path.join(output, fname))
+    return missing, categorical
+
+# missing_handler()
+def missing_handler(df=None, missing=None, output=r'../public/output', fname='missing_variables.png'):
+    """ Handle features with missing values
+
+    Args:
+        df: DataFrame
+        missing: set, feature names with missing values
+        output: path 
+        fname: output file name
+
+    Returns:
+        
+    """
+    return df
+
+# categorical_conversion()
+def categorical_conversion(df=None, categorical_feature=None):
+    return df
