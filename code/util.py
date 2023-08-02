5ab923a36cb5931a6e0cb3e2e621707c0ca214c0
XinxieWu <xinxiewu@gmail.com>
2023-08-02 19:19:41 -0400

    EDA 3

--- code/util.py
+++ code/util.py
@@ -113,9 +113,40 @@ def missing_handler(df=None, missing=None, output=r'../public/output', fname='mi
     Returns:
         
     """
-    df_copy = df.copy()
     for miss in missing:
-        pass
+        df_0 = df.copy()
+        if miss == 'Item_Weight':
+            key = 'Item_Identifier'
+            identifier_cnt_0 = getattr(df, key).unique()
+            identifier_cnt_1 = getattr(df[getattr(df, miss).isna()==False], key).unique()
+            identifier_unique = []
+            for id in identifier_cnt_0:
+                if id not in identifier_cnt_1:
+                    identifier_unique.append(id)
+            df_0[key] = pd.factorize(getattr(df,key))[0]
+            grouped = pd.DataFrame(df_0.groupby(key)[miss].nunique().reset_index())
+            sns.set()
+            fig, axes = plt.subplots(1, 5, figsize=(30, 6))
+            ax = axes.flatten()
+            overall = sns.lineplot(data=grouped, x=key, y=miss, ax=ax[0])
+            overall.set(xlabel=f"{key}", ylabel=f"Distinct Values of {miss}", title=f"Line Chart of {miss} vs {key}")
+            for i, item in enumerate(identifier_unique):
+                fat_cont = ['Regular','reg'] if item != 'FDK57' else ['LF', 'Low Fat']
+                if item == 'FDN52':
+                    item_type = 'Frozen Foods'
+                elif item == 'FDK57':
+                    item_type = 'Snack Foods'
+                elif item == 'FDE52':
+                    item_type = 'Dairy'
+                else:
+                    item_type = 'Baking Goods'
+                temp = df.query(f"Item_Fat_Content in {fat_cont} & Item_Type == '{item_type}'")
+                temp_0 = sns.histplot(getattr(temp, miss),kde=True,color='purple',ax=ax[i+1])
+                temp_0.set(xlabel=f"{key} of {fat_cont} & {item_type}", ylabel=f"Weight Distribution", 
+                           title=f"Mean: {format(getattr(temp,miss).mean(),'.2f')}, Median: {format(getattr(temp,miss).median(),'.2f')}, STD: {format(getattr(temp,miss).std(),'.2f')}")
+
+            plt.tight_layout()
+            plt.savefig(os.path.join(output, fname))
     return df
 
 # categorical_conversion()
